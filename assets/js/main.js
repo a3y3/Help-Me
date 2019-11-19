c3a0860bc8ff546db112d15ec06f79872eb7d28c
a3y3 <sohamssd@gmail.com>
2019-11-19 11:36:49 -0500

    Fetch orgTypes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,38 @@
+"use strict";
 $(function () {
-    "use-strict";
     let helpMe = (function () {
+        let proxy_url;
+        let getOrgTypes = function () {
+            let url = `/OrgTypes`;
+            $.ajax({
+                "url": proxy_url,
+                "data": { path: url },
+                "success": function (data) {
+                    return parseOrgTypes(data);
+                },
+                "error": function (xhr, data, err) {
+                    console.error( `Error:`, xhr, data, err);
+                }
+            });
+        }
+        function parseOrgTypes(xmlData){
+            let orgTypes = {}
+            $("row", xmlData).each(function(data){
+                let id = parseInt($("typeId", this).text());
+                let type = $("type", this).text()
+                orgTypes[id] = type;
+            });
+            return orgTypes;
+        }
         return {
             init: function () {
-                return "Hello World!";
+                // init data members
+                proxy_url = `https://people.rit.edu/dmgics/754/23/proxy.php`;
+
+                // init member functions
+                getOrgTypes();
             }
         }
     })();
+    helpMe.init();
 });
