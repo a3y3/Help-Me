3025cb82b9c700622e203bb4cec40cec00c6be41
a3y3 <sohamssd@gmail.com>
2019-11-19 20:13:37 -0500

    Display search results in table

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,7 +12,7 @@ $(function () {
                     let orgTypes = parseOrgTypes(data);
                     let select = $("#select-orgType");
                     $.each(orgTypes, function (key, value) {
-                        select.append($("<option></option>").attr("value", key).text(value));
+                        select.append($("<option></option>").attr("value", value).text(value));
                     });
                 },
                 "error": function (xhr, data, err) {
@@ -31,7 +31,7 @@ $(function () {
             return orgTypes;
         }
 
-        function setSelectSingle(url, select, field){
+        function setSelectSingle(url, select, field) {
             $.ajax({
                 "url": proxy_url,
                 "data": { path: url },
@@ -46,7 +46,7 @@ $(function () {
                 }
             });
         }
-        
+
 
         function parseXmlDataSingle(xmlData, field) {
             let states = []
@@ -64,7 +64,7 @@ $(function () {
             setSelectSingle(url, select, fieldName);
         }
 
-        function setCities(){
+        function setCities() {
             let url = `/Cities`;
             let select = $("#select-cities");
             let fieldName = "city";
@@ -75,13 +75,91 @@ $(function () {
             setOrgTypes();
             setStates();
             setCities();
+            $("#form-clear").on("click", function () {
+                $('#form-search')[0].reset();
+            });
+        }
+
+        function handleSearch() {
+            $("#form-search").on("submit", function (event) {
+                event.preventDefault();
+                //First, clear prev results
+                $("#search-results").children().not(":first").remove();
+
+                let params = $(this).serialize();
+                let searchUrl = "/Organizations";
+                $.ajax({
+                    "url": proxy_url,
+                    "data": { "path": searchUrl + "?" + params },
+                    "success": function (data) {
+                        if ($("row", data).length != 0) {
+                            showInTable(data);
+
+                        }
+                        else {
+                            let div = $("#search-results");
+                            addNotFound(div);
+                        }
+                    },
+                    "error": function (xhr, data, err) {
+                        console.error(`Error:`, xhr, data, err);
+                    }
+                });
+            });
+        }
+
+        function showInTable(data) {
+            let table = getTable();
+            $("row", data).each(function () {
+                let tr = document.createElement("tr");
+                let results = []
+                // results.push($("OrganizationID", this).text());
+                results.push($("type", this).text());
+                results.push($("Name", this).text());
+                results.push($("city", this).text());
+                results.push($("zip", this).text());
+                results.push($("CountyName", this).text());
+                results.push($("State", this).text());
+                $(results).each(function (i) {
+                    let td = document.createElement("td");
+                    td.append(results[i]);
+                    tr.append(td);
+                });
+                table.append(tr);
+            });
+            $("#search-results").append(table);
+        }
+
+        function getTable() {
+            let table = document.createElement("table");
+            let thead = document.createElement("thead");
+            let tbody = document.createElement("tbody");
+            let headers = ["Type", "Name", "City", "Zip", "County", "State"];
+            $(headers).each(function (i) {
+                let th = document.createElement("th");
+                th.append(headers[i]);
+                thead.append(th);
+            });
+            table.append(thead);
+            table.append(tbody);
+
+            $(table).addClass("table");
+            $(table).addClass("table-striped")
+            $(thead).addClass("thead-dark");
+            return table;
         }
         return {
             init: function () {
                 // init data members
                 proxy_url = `https://people.rit.edu/dmgics/754/23/proxy.php`;
                 initForm();
+                handleSearch();
             }
+        }
+
+        function addNotFound(div){
+            let text = document.createTextNode("No results found");
+            div.append(text);
         }
     })();
     helpMe.init();
